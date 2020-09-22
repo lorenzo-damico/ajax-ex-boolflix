4ebeb9c18d3fcd99cc78762976f72f155559ee3c
lorenzo-damico <loredam@hotmail.it>
2020-09-22 16:10:11 +0200

    Aggiunta chiamata ajax al database e stampa a schermo dei risultati

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // Milestone 1
 // Creare un layout base con una searchbar (una input e un button) in cui
 // possiamo scrivere completamente o parzialmente il nome di un film.
-// Possiamo, cliccando il  bottone, cercare sull’API tutti i film che contengono
+// Possiamo, cliccando il bottone, cercare sull’API tutti i film che contengono
 // ciò che ha scritto l’utente.
 // Vogliamo dopo la risposta dell’API visualizzare a schermo i seguenti valori
 // per ogni film trovato:
@@ -18,12 +18,58 @@ $(document).ready(function () {
   $("#search-button").click(
     function () {
 
+      // Svuoto la lista di film.
+      $("#movies-list").html("");
+
       // Creo una variabile con il contenuto dell'input.
       var searchInput = $("#search-films").val();
 
       // Pulisco il campo di input.
       $("#search-films").val("");
-      
+
+      // Effettuo la chiamata ajax all'API del movie database, per ottenere
+      // le schede dei film.
+      $.ajax(
+        {
+          "url":"https://api.themoviedb.org/3/search/movie",
+          "data": {
+            "api_key": "04bbabd51c895f6f8040168aa7e1cd41",
+            "query": searchInput,
+            "language": "it-IT",
+            "include_adult": false
+          },
+          "method": "GET",
+          "success": function (data) {
+
+            // Salvo la lista dei film in un array.
+            var listaFilm = data.results;
+
+            // Genero il template con handlebars.
+            var source = $("#film-template").html();
+            var template = Handlebars.compile(source);
+
+            // Eseguo un ciclo sull'array dei film per stamparli a schermo.
+            for (var i = 0; i < listaFilm.length; i++) {
+
+              // Genero l'oggetto context da stampare.
+              var context = {
+                "title": listaFilm[i].title,
+                "original_title": listaFilm[i].original_title,
+                "original_language": listaFilm[i].original_language,
+                "vote_average": listaFilm[i].vote_average
+              };
+
+              // Compilo il template e lo aggiungo nella lista film.
+              var html = template(context);
+              $("#movies-list").append(html);
+            }
+          },
+          "error": function (err) {
+            alert("Errore!");
+          }
+        }
+      );
+
     }
   );
 
