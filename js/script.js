3af2f2b407c3e0dda1e15ff3c7124efd57538adf
lorenzo-damico <loredam@hotmail.it>
2020-09-22 16:40:17 +0200

    pulizia codice e aggiunta ricerca con tasto invio

--- js/script.js
+++ js/script.js
@@ -12,65 +12,88 @@
 
 $(document).ready(function () {
 
+  // FUNZIONI
+
+  // Funzione di ricerca e stampa dei film.
+  function renderFilms() {
+
+    // Svuoto la lista di film.
+    $("#movies-list").html("");
+
+    // Creo una variabile con il contenuto dell'input.
+    var searchInput = $("#search-films").val();
+
+    // Pulisco il campo di input.
+    $("#search-films").val("");
+
+    // Effettuo la chiamata ajax all'API del movie database, per ottenere
+    // le schede dei film.
+    $.ajax(
+      {
+        "url":"https://api.themoviedb.org/3/search/movie",
+        "data": {
+          "api_key": "04bbabd51c895f6f8040168aa7e1cd41",
+          "query": searchInput,
+          "language": "it-IT",
+          "include_adult": false
+        },
+        "method": "GET",
+        "success": function (data) {
+
+          // Salvo la lista dei film in un array.
+          var listaFilm = data.results;
+
+          // Genero il template con handlebars.
+          var source = $("#film-template").html();
+          var template = Handlebars.compile(source);
+
+          // Eseguo un ciclo sull'array dei film per stamparli a schermo.
+          for (var i = 0; i < listaFilm.length; i++) {
+
+            // Genero l'oggetto context da stampare.
+            var context = {
+              "title": listaFilm[i].title,
+              "original_title": listaFilm[i].original_title,
+              "original_language": listaFilm[i].original_language,
+              "vote_average": listaFilm[i].vote_average
+            };
+
+            // Compilo il template e lo aggiungo nella lista film.
+            var html = template(context);
+            $("#movies-list").append(html);
+          }
+        },
+        "error": function (err) {
+          alert("Errore!");
+        }
+      }
+    );
+  }
+
+  // FINE FUNZIONI
+
   // EVENTI
 
-  // Aggiungo un evento al click sul bottone.
+  // 1. Aggiungo un evento al click sul bottone.
   $("#search-button").click(
     function () {
 
-      // Svuoto la lista di film.
-      $("#movies-list").html("");
-
-      // Creo una variabile con il contenuto dell'input.
-      var searchInput = $("#search-films").val();
-
-      // Pulisco il campo di input.
-      $("#search-films").val("");
-
-      // Effettuo la chiamata ajax all'API del movie database, per ottenere
-      // le schede dei film.
-      $.ajax(
-        {
-          "url":"https://api.themoviedb.org/3/search/movie",
-          "data": {
-            "api_key": "04bbabd51c895f6f8040168aa7e1cd41",
-            "query": searchInput,
-            "language": "it-IT",
-            "include_adult": false
-          },
-          "method": "GET",
-          "success": function (data) {
-
-            // Salvo la lista dei film in un array.
-            var listaFilm = data.results;
-
-            // Genero il template con handlebars.
-            var source = $("#film-template").html();
-            var template = Handlebars.compile(source);
-
-            // Eseguo un ciclo sull'array dei film per stamparli a schermo.
-            for (var i = 0; i < listaFilm.length; i++) {
-
-              // Genero l'oggetto context da stampare.
-              var context = {
-                "title": listaFilm[i].title,
-                "original_title": listaFilm[i].original_title,
-                "original_language": listaFilm[i].original_language,
-                "vote_average": listaFilm[i].vote_average
-              };
-
-              // Compilo il template e lo aggiungo nella lista film.
-              var html = template(context);
-              $("#movies-list").append(html);
-            }
-          },
-          "error": function (err) {
-            alert("Errore!");
-          }
-        }
-      );
-
+      // Uso la funzione di ricerca e stampa dei risultati.
+      renderFilms();
     }
   );
+
+  // 2. Aggiungo un evento alla pressione del tasto invio.
+  $("#search-films").keyup(
+    function () {
+
+      // Se premo il tasto invio, cerco e stampo.
+      if (event.which == 13) {
+        renderFilms();
+      }
+    }
+  );
+
+  // FINE EVENTI
 
 });
