eb9a4112456bdbaaaf920cde5cb9f247305f28d6
lorenzo-damico <loredam@hotmail.it>
2020-09-23 12:20:45 +0200

    Refactoring sulle funzioni

--- js/script.js
+++ js/script.js
@@ -14,26 +14,51 @@ $(document).ready(function () {
 
   // FUNZIONI
 
-  // Funzione di ricerca e stampa dei film.
-  function renderFilms() {
+  // Funzione di pulizia del campo input e della lista film.
+  function resetSearch() {
 
     // Svuoto la lista di film.
     $("#movies-list").html("");
 
-    // Creo una variabile con il contenuto dell'input.
-    var searchInput = $("#search-films").val();
-
     // Pulisco il campo di input.
     $("#search-films").val("");
+  }
+
+  // Funzione che stampa a schermo.
+  function renderMovies(arrayDatabase) {
+
+    // Genero il template con handlebars.
+    var source = $("#film-template").html();
+    var template = Handlebars.compile(source);
+
+    // Eseguo un ciclo sull'array dei film per stamparli a schermo.
+    for (var i = 0; i < arrayDatabase.length; i++) {
+
+      // Genero l'oggetto context da stampare.
+      var context = {
+        "title": arrayDatabase[i].title,
+        "original_title": arrayDatabase[i].original_title,
+        "original_language": arrayDatabase[i].original_language,
+        "vote_average": arrayDatabase[i].vote_average
+      };
+
+      // Compilo il template e lo aggiungo nella lista film.
+      var html = template(context);
+      $("#movies-list").append(html);
+    }
+  }
+
+  // Funzione di ricerca e stampa dei film.
+  function getMovies(search) {
 
     // Effettuo la chiamata ajax all'API del movie database, per ottenere
     // le schede dei film.
     $.ajax(
       {
-        "url":"https://api.themoviedb.org/3/search/movie",
+        "url": "https://api.themoviedb.org/3/search/movie",
         "data": {
           "api_key": "04bbabd51c895f6f8040168aa7e1cd41",
-          "query": searchInput,
+          "query": search,
           "language": "it-IT",
           "include_adult": false
         },
@@ -43,25 +68,9 @@ $(document).ready(function () {
           // Salvo la lista dei film in un array.
           var listaFilm = data.results;
 
-          // Genero il template con handlebars.
-          var source = $("#film-template").html();
-          var template = Handlebars.compile(source);
+          // Eseguo la funzione di stampa.
+          renderMovies(listaFilm);
 
-          // Eseguo un ciclo sull'array dei film per stamparli a schermo.
-          for (var i = 0; i < listaFilm.length; i++) {
-
-            // Genero l'oggetto context da stampare.
-            var context = {
-              "title": listaFilm[i].title,
-              "original_title": listaFilm[i].original_title,
-              "original_language": listaFilm[i].original_language,
-              "vote_average": listaFilm[i].vote_average
-            };
-
-            // Compilo il template e lo aggiungo nella lista film.
-            var html = template(context);
-            $("#movies-list").append(html);
-          }
         },
         "error": function (err) {
           alert("Errore!");
@@ -72,14 +81,21 @@ $(document).ready(function () {
 
   // FINE FUNZIONI
 
+
   // EVENTI
 
   // 1. Aggiungo un evento al click sul bottone.
   $("#search-button").click(
     function () {
 
+      // Creo una variabile con il contenuto dell'input.
+      var searchInput = $("#search-films").val();
+
+      // Pulisco input e lista.
+      resetSearch();
+
       // Uso la funzione di ricerca e stampa dei risultati.
-      renderFilms();
+      getMovies(searchInput);
     }
   );
 
@@ -89,7 +105,15 @@ $(document).ready(function () {
 
       // Se premo il tasto invio, cerco e stampo.
       if (event.which == 13) {
-        renderFilms();
+
+        // Creo una variabile con il contenuto dell'input.
+        var searchInput = $("#search-films").val();
+
+        // Pulisco input e lista.
+        resetSearch();
+
+        // Uso la funzione di ricerca e stampa dei risultati.
+        getMovies(searchInput);
       }
     }
   );
